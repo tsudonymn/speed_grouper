3d6158c6ae6c69b5efe97d1867040d4d044d418a
Chris <tsudonymn@gmail.com>
2022-02-24 22:33:41 -0500

    Updated to use intersection instead of equality

--- test/test_speed_grouper.py
+++ test/test_speed_grouper.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from assertpy import assert_that
 
 from grouper import generate_person_list
+from person import Person
 from speed_grouper import SpeedGrouper
 
 
@@ -14,13 +15,34 @@ class Test(TestCase):
         groups_per_round = len(all_the_people) / num_per_group
         grouper = SpeedGrouper(all_the_people, num_per_group)
 
-        round1_expected_groups = [('Person1', 'Person2', 'Person3'), ('Person4', 'Person5', 'Person6'),
-                                  ('Person7', 'Person8', 'Person9')]
+        p1 = Person('1')
+        p2 = Person('2')
+        p3 = Person('3')
+        p4 = Person('4')
+        p5 = Person('5')
+        p6 = Person('6')
+        p7 = Person('7')
+        p8 = Person('8')
+        p9 = Person('9')
+        p10 = Person('10')
+        p11 = Person('11')
+        p12 = Person('12')
+        p13 = Person('13')
+        p14 = Person('14')
+        p15 = Person('15')
+        g123 = (p1, p2, p3)
+        g456 = (p4, p5, p6)
+        g789 = (p7, p8, p9)
+        g101112 = (p10, p11, p12)
+        g131415 = (p13, p14, p15)
+        round1_expected_groups = [g123, g456, g789, g101112, g131415 ]
         round = grouper.build_next_round()
         assert_that(round.number).is_equal_to(1)
         assert_that(len(grouper.rounds)).is_equal_to(1)
         assert_that(len(round.groups)).is_equal_to(groups_per_round)
+        assert_that(len(round.groups)).is_equal_to(len(round1_expected_groups))
         # assert_that(round.groups).is_equal_to(round1_expected_groups)
+        assert_that(set(round.groups).intersection(set(round1_expected_groups))).is_equal_to(set())
         #
         # expected2 = []
         # round2 = grouper.build_next_round()
